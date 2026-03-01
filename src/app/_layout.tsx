a54555d1fae1452c2f480445b9831d3c0295b601
JJW1643 <josh.woodward1@outlook.com>
2026-03-01 17:01:53 +0000

    initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+// Import the dark theme from react navigation and wrap the entire app in it
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+
+// This is the root layout of the app, it wraps all the screens in the app and sets the theme to dark
+export default function RootLayout() {
+    
+    // Set the layout of the index page to be the events page
+    return (
+    <ThemeProvider value={DarkTheme}>
+        <Stack>
+
+            
+
+            <Stack.Screen 
+            name ="index" 
+            options={{ title: 'Events', headerLargeTitle: true }}
+            />
+        </Stack>
+        </ThemeProvider>
+    );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+
+// This is the entry point of the application. It defines the main component that will be rendered when the app starts. English - Create a screen called Home containing the text 'Home Screen'
+
+import { View, Text } from 'react-native';
+
+
+// The Home component is a simple functional component that renders a view with some text. This will be the main screen of the app.
+export default function Home() {
+    // This is what the component actually displays on screen. Everything inside here is what the user will see when they open the app.
+    return (
+        <View>
+            <Text>Home Screen</Text>    
+        </View>
+    );
+}
